5ddb99ef9c5200ff47ea3fd7228ef467fd32c9c4
Dillona25 <Dillyb00by49@gmail.com>
2024-04-29 13:02:58 -0500

    Added completed reviews section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,7 @@ function App() {
       </div>
       <OurServices />
       <Reviews />
-      {/* <ActionCall /> */}
+      <ActionCall />
     </div>
   );
 }
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,15 +1,23 @@
 import { ReviewsContainer } from "./ReviewsContainer";
-import { ReviewsTitle } from "./ReviewsTitle";
 
 export const Reviews = () => {
   return (
-    <>
-      <ReviewsTitle />
+    <section className="bg-white">
+      <div className="flex flex-col gap-7">
+        <div className="flex flex-col">
+          <h1 className="leading-[50px]">Dont just take</h1>
+          <span className="leading-[50px]">our word...</span>
+          <p className="mt-5 max-w-[330px]">
+            We've done work for over 5 trusted telecommunications companies.
+            Serving over 300,000 customers. See what they have said.{" "}
+          </p>
+        </div>
+      </div>
       <div className="flex flex-col gap-5 items-center py-[50px]">
         <ReviewsContainer />
         <ReviewsContainer />
         <ReviewsContainer />
       </div>
-    </>
+    </section>
   );
 };
