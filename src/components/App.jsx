61e5cd4a608b7bae398793a392513b972667acde
Dillona25 <Dillyb00by49@gmail.com>
2024-05-06 19:38:12 -0500

    Fix-mobile: Added logic to close navbar on new route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,20 @@ import { OurServices } from "./OurServices/OurServices";
 import { ActionCall } from "./ActionCall/ActionCall";
 import { Reviews } from "./Reviews/Reviews";
 import { Footer } from "./Footer/Footer";
-import { BrowserRouter as ReactRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as ReactRouter,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Contact } from "../routes/Contact";
 import { About } from "../routes/About";
 import { UndergroundServices } from "./UndergroundServices/UndergroundServices";
 import { Careers } from "../routes/Careers";
 
 function App() {
-  const [activeModal, setActiveModal] = useState("");
+  const [activeModal, setActiveModal] = useState(false);
+  const location = useLocation();
 
   const handleSideBarMenu = () => {
     setActiveModal("SideBarMenu");
@@ -23,76 +29,78 @@ function App() {
     setActiveModal("");
   };
 
+  useEffect(() => {
+    closeModal(); // Reset modal state when route changes
+  }, [location.pathname]);
+
   return (
-    <ReactRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="bg-[#f4f4f4]">
-              <div className="relative bg-Hero bg-cover bg-center h-screen">
-                <div className="absolute inset-0 flex flex-col">
-                  <Navbar handleSideBarMenu={handleSideBarMenu} />
-                  {activeModal === "SideBarMenu" && (
-                    <MobileNavbar closeModal={closeModal} />
-                  )}
-                  <div className="flex-1 flex items-center justify-center">
-                    <div className="flex flex-col items-center">
-                      <Hero />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <OurServices />
-              <UndergroundServices />
-              <ActionCall />
-              <Reviews />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/Contact"
-          element={
-            <div className="h-screen relative">
-              <div className=" bg-Contact bg-cover bg-center absolute h-1/2">
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="bg-[#f4f4f4]">
+            <div className="relative bg-Hero bg-cover bg-center h-screen">
+              <div className="absolute inset-0 flex flex-col">
                 <Navbar handleSideBarMenu={handleSideBarMenu} />
                 {activeModal === "SideBarMenu" && (
                   <MobileNavbar closeModal={closeModal} />
                 )}
-                <Contact />
+                <div className="flex-1 flex items-center justify-center">
+                  <div className="flex flex-col items-center">
+                    <Hero />
+                  </div>
+                </div>
               </div>
             </div>
-          }
-        />
-        <Route
-          path="/About"
-          element={
-            <>
+            <OurServices />
+            <UndergroundServices />
+            <ActionCall />
+            <Reviews />
+            <Footer />
+          </div>
+        }
+      />
+      <Route
+        path="/Contact"
+        element={
+          <div className="h-screen relative">
+            <div className=" bg-Contact bg-cover bg-center absolute h-1/2">
               <Navbar handleSideBarMenu={handleSideBarMenu} />
               {activeModal === "SideBarMenu" && (
                 <MobileNavbar closeModal={closeModal} />
               )}
-              <About />
-              {/* <Footer /> */}
-            </>
-          }
-        />
-        <Route
-          path="/Careers"
-          element={
-            <>
-              <Navbar handleSideBarMenu={handleSideBarMenu} />
-              {activeModal === "SideBarMenu" && (
-                <MobileNavbar closeModal={closeModal} />
-              )}
-              <Careers />
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
-    </ReactRouter>
+              <Contact />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/About"
+        element={
+          <>
+            <Navbar handleSideBarMenu={handleSideBarMenu} />
+            {activeModal === "SideBarMenu" && (
+              <MobileNavbar closeModal={closeModal} />
+            )}
+            <About />
+            {/* <Footer /> */}
+          </>
+        }
+      />
+      <Route
+        path="/Careers"
+        element={
+          <>
+            <Navbar handleSideBarMenu={handleSideBarMenu} />
+            {activeModal === "SideBarMenu" && (
+              <MobileNavbar closeModal={closeModal} />
+            )}
+            <Careers />
+            <Footer />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
